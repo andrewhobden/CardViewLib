a3a8e3bfb5a31ff0549d7595b820bab26e822b21
Andrew Hobden <andrew@bigmeandata.com>
2022-10-21 18:49:06 +0800

    initial update

--- Sources/CardViewLib/CardViewLib.swift
+++ Sources/CardViewLib/CardViewLib.swift
@@ -1,6 +1,15 @@
-public struct CardViewLib {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
+@available(iOS 14, macOS 11.0, *)
+public struct CardView : View {
+    @State var CardName = "A walk in the trees"
+    @State var CardAuthor = "Created by Andrew"
+        
+    public var body: some View {
+        VStack {
+            Text(CardName)
+            Text(CardAuthor)
+        }.border(Color.black)
+            
     }
 }
